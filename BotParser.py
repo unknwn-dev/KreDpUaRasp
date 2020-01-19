033648f2859179e57ccef1c617923bd8924ac263
Monotyper04 <57369007+Monotyper04@users.noreply.github.com>
2020-01-19 19:23:43 +0200

    Update BotParser.py

--- BotParser.py
+++ BotParser.py
@@ -3,6 +3,8 @@ import telebot
 import urllib.request
 from bs4 import BeautifulSoup
 
+requests = 0
+
 def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
@@ -58,10 +60,15 @@ def ResloaDAct(message):
     bot.reply_to(message, "Updated")
     print(str(date))
 
+@bot.message_handler(commands=['StatsByDay'])
+def Stats(message):
+    answ = "Запросов за этот день :" + str(requests)
+    bot.reply_to(message,answ)
 
 @bot.message_handler(content_types = ["text"])
 def send_raspisanye(message):
   answInt = 0
+  requests = requests + 1
   try:
     if int(message.text) == 50:
       answInt=12
